20f03bf5f9795e100a10fb845ec0a64eee10c3e4
shamshad-dev27 <shamshadahmad843127@gmail.com>
2026-01-15 22:54:49 +0530

    Other_commit

--- server/index.js
+++ server/index.js
@@ -1,10 +1,47 @@
 const http=require('http');
-const port=3021;
+const { join } = require('path');
+const port=3032;
 const hostname="localhost"
 const server=http.createServer((req,res)=>{
-    res.statusCode=500;
-    res.setHeader('Content-Type','Application/json');
-    res.end(JSON.stringify({ error : 'server-error'}))
+    if(req.url=='/'){
+        res.statusCode=200;
+    res.setHeader('Content-Type','text-plain');
+    res.end('Welcome to node js server');
+    } 
+    else if(req.url=='/about'){
+         res.statusCode=200;
+    res.setHeader('Content-Type','text-plain');
+    res.end('About!');
+    }else if(req.url=='/contact'){
+        res.statusCode=200;
+    res.setHeader('Content-Type','text-plain');
+    res.end('contace!'); 
+    }else if(req.url=='/product'){
+        const option={
+            hostname:'fakestoreapi.com',
+            path:'/products/1',
+            method: 'GET'
+        }
+        
+        const reqApi=http.request(option,(resApi)=>{
+              resApi.on("data",(data)=>{
+                 res.statusCode=200;
+               res.setHeader('Content-Type','Application/json');
+               res.end((data.toString()));
+              })
+        })
+        
+        
+        reqApi.on("error",()=>{
+            console.log(e);
+        })
+        reqApi.end();
+        
+    }else{
+         res.statusCode=500;
+    res.setHeader('Content-Type','text-plain');
+    res.end('ERROR!');
+    }
 })
 
 server.listen(port,()=>{
--- server/request.js
+++ server/request.js
@@ -1,5 +1,4 @@
 const http=require('http');
-const { hostname } = require('os');
 
 const option={
     hostname:'fakestoreapi.com',
