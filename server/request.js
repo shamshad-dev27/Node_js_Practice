83a6f81d7c266c6949ba6fcf37190b8e0bd3858f
shamshad-dev27 <shamshadahmad843127@gmail.com>
2026-01-15 19:59:47 +0530

    second-commit

---
+++ server/request.js
@@ -0,0 +1,20 @@
+const http=require('http');
+const { hostname } = require('os');
+
+const option={
+    hostname:'fakestoreapi.com',
+    path:'/products/1',
+    method: 'GET'
+}
+
+const reqApi=http.request(option,(resApi)=>{
+      resApi.on("data",(data)=>{
+        console.log(data.toString())
+      })
+})
+
+
+reqApi.on("error",()=>{
+    console.log(e);
+})
+reqApi.end();
